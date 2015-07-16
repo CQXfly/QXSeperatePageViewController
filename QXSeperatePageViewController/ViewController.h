9e106f3b2f04c36ed422a4354502b6bb04bb5786
CQXfly <905799827@qq.com>
2015-07-16 16:24:21 +0800

    firstCommit

---
+++ QXSeperatePageViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QXSeperatePageViewController
+//
+//  Created by 崇庆旭 on 15/7/16.
+//  Copyright (c) 2015年 崇庆旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QXSeperatePageViewController/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  QXSeperatePageViewController
+//
+//  Created by 崇庆旭 on 15/7/16.
+//  Copyright (c) 2015年 崇庆旭. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
